32f0512de9920d4446b44498d702abb6ec110b10
nakano <nakano2001a@gmail.com>
2025-11-30 15:44:45 +0900

    roguin

--- frontend/src/components/organisms/Header.tsx
+++ frontend/src/components/organisms/Header.tsx
@@ -1,10 +1,13 @@
+import { toast } from 'react-toastify';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HeaderMain } from '@/components/atoms/HeaderMain';
 import { HeaderNav } from '@/components/atoms/HeaderNav';
 import { Menu } from '@/components/molecules/Menu';
 import { useLogout } from '@/hooks/useLogout';
 import type { Option } from '@/interface/ui';
 import { ROUTES } from '@/constants/routes';
+import { handleApiError } from '@/utils/handleApiError';
 import { useLoginContext } from '@/hooks/useLoginContext';
 
 const HeaderOptions: Option[] = [
@@ -15,11 +18,23 @@ const HeaderOptions: Option[] = [
 ];
 
 export const Header: React.FC = () => {
+  const navigate = useNavigate();
   const { logout, loading } = useLogout();
   const main = HeaderOptions[0];
   const { passwordUpdateRequired } = useLoginContext();
   const navOptions = HeaderOptions.slice(1);
   const [open, setOpen] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      const res = await logout();
+      toast.success(res.message);
+      navigate(ROUTES.HOME);
+    } catch (err: any) {
+      handleApiError(err, navigate);
+    }
+  };
+
   return (
     <header className="border-b">
       <div className="px-5 py-4 flex flex-col sm:flex-row sm:justify-between items-center">
@@ -28,7 +43,7 @@ export const Header: React.FC = () => {
         <div className="flex flex-col sm:flex-row items-center">
           <HeaderNav
             options={navOptions}
-            onLogout={logout}
+            onLogout={handleLogout}
             loading={loading}
             passwordUpdateRequired={passwordUpdateRequired}
           />
