621b1f2550ff21a347db369e8cc28e641b9acb26
nakano <nakano2001a@gmail.com>
2025-12-22 22:12:10 +0900

    小分類　リダックス

---
+++ frontend/src/features/minorCategory/slice.ts
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { minorCategoryApi } from '@/features/minorCategory';
+import type { MinorCategory } from '@/features/minorCategory';
+
+//Redux Action
+export const fetchMinorCategories = createAsyncThunk('minorCategories/fetchAll', async () => {
+  const res = await minorCategoryApi.index();
+  return res;
+});
+
+//stateを定義
+type MinorCategoryState = {
+  data: MinorCategory[];
+  loading: boolean;
+};
+
+//初期state
+const initialState: MinorCategoryState = {
+  data: [],
+  loading: false,
+};
+
+export const minorCategorySlice = createSlice({
+  name: 'minorCategories',
+  initialState,
+  //stateは変更しないので空
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      //pending取得中
+      .addCase(fetchMinorCategories.pending, (state) => {
+        state.loading = true;
+      })
+      //fulfilled取得成功
+      .addCase(fetchMinorCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload.data ?? [];
+      })
+      //rejected取得失敗
+      .addCase(fetchMinorCategories.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export default minorCategorySlice.reducer;
--- frontend/src/hooks/store.ts
+++ frontend/src/hooks/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import departmentReducer from '@/features/department/slice';
 import categoryReducer from '@/features/category/slice';
+import minorCategoryReducer from '@/features/minorCategory/slice';
 
 export const store = configureStore({
   reducer: {
     departments: departmentReducer,
     categories: categoryReducer,
+    minorCategories: minorCategoryReducer,
     // 他の slice...
   },
 });
