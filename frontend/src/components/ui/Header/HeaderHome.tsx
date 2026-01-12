1a13b9fbd3944a82eaaf2c89b703c64603ff4e5f
noemaru1011 <166391423+noemaru1011@users.noreply.github.com>
2026-01-13 03:00:58 +0900

    Update hover background color for HeaderHome link

--- frontend/src/components/ui/Header/HeaderHome.tsx
+++ frontend/src/components/ui/Header/HeaderHome.tsx
@@ -13,7 +13,7 @@ export const HeaderHome = ({ to, label }: Props) => {
     <Link
       to={to}
       aria-current={location.pathname === to ? 'page' : undefined}
-      className="flex items-center font-bold mb-2 sm:mb-0 text-gray-800 p-2 transition-colors rounded-lg hover:bg-gray-100"
+      className="flex items-center font-bold mb-2 sm:mb-0 text-gray-800 transition-colors rounded-lg  transition-colors hover:bg-gray-300"
     >
       <Home aria-hidden="true" className="w-5 h-5 mr-2 text-gray-600" />
       {label}
