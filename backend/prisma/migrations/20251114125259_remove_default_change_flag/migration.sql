57c3dc124d19c52dd7931626bc65cbe9e3c1120d
nakano <nakano2001a@gmail.com>
2026-01-18 23:53:47 +0900

    DB

--- backend/prisma/migrations/20251114125259_remove_default_change_flag/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `defaultChangeFlag` on the `StudentPassword` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "StudentPassword" DROP COLUMN "defaultChangeFlag";
