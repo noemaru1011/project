e6ec36a9ac9e793a584f265f4b19594cbfb68b07
nakano <nakano2001a@gmail.com>
2026-01-19 00:14:38 +0900

    ログ

--- backend/src/repositories/baseRepository.ts
+++ backend/src/repositories/baseRepository.ts
@@ -1,4 +1,35 @@
 import { Prisma, PrismaClient } from '@prisma/client';
+import { logger } from '@/utils/log/logger';
+
+export const prisma = new PrismaClient({
+  log: [
+    { emit: 'event', level: 'query' },
+    { emit: 'event', level: 'info' },
+    { emit: 'event', level: 'warn' },
+    { emit: 'event', level: 'error' },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  let maskedParams = e.params;
+
+  try {
+    const paramsArray = JSON.parse(e.params);
+    maskedParams = JSON.stringify(
+      paramsArray.map((p: any) => ({
+        ...p,
+        password: p.password ? '***' : p.password,
+      })),
+    );
+  } catch {}
+
+  logger.info('Prisma query executed', {
+    type: 'prisma-query',
+    sql: e.query,
+    params: maskedParams,
+    duration: e.duration,
+  });
+});
 
 export abstract class BaseRepository {
   constructor(protected prisma: Prisma.TransactionClient | PrismaClient) {}
--- backend/src/utils/log/logger.ts
+++ backend/src/utils/log/logger.ts
@@ -1,7 +1,6 @@
 import winston from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-// 日本時刻
 const jstTimestamp = () =>
   new Date().toLocaleString('ja-JP', {
     timeZone: 'Asia/Tokyo',
@@ -13,34 +12,53 @@ const jstTimestamp = () =>
     second: '2-digit',
   });
 
-export const createLogger = (logDir: string) => {
-  return winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({ format: jstTimestamp }),
-      winston.format.errors({ stack: true }),
-      winston.format.splat(),
-      winston.format.json(),
-    ),
-    transports: [
-      new DailyRotateFile({
-        dirname: logDir,
-        filename: 'error-%DATE%.log',
-        datePattern: 'YYYY-MM-DD',
-        level: 'error',
-        maxFiles: '14d',
-      }),
-      new DailyRotateFile({
-        dirname: logDir,
-        filename: 'access-%DATE%.log',
-        datePattern: 'YYYY-MM-DD',
-        level: 'info',
-        maxFiles: '14d',
-      }),
-    ],
-  });
-};
-
 const logDir = process.env.NODE_ENV === 'development' ? 'logs-test' : 'logs';
 
-export const logger = createLogger(logDir);
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: jstTimestamp }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json(),
+  ),
+  transports: [
+    // エラーログ
+    new DailyRotateFile({
+      dirname: logDir,
+      filename: 'error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'error',
+      maxFiles: '14d',
+    }),
+    // アクセスログ
+    new DailyRotateFile({
+      dirname: logDir,
+      filename: 'access-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'info',
+      maxFiles: '14d',
+      format: winston.format((info) =>
+        info.type !== 'resource' && info.type !== 'prisma-query' ? info : false,
+      )(),
+    }),
+    // リソースログ
+    new DailyRotateFile({
+      dirname: logDir,
+      filename: 'resource-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'info',
+      maxFiles: '14d',
+      format: winston.format((info) => (info.type === 'resource' ? info : false))(),
+    }),
+    // Prisma クエリ専用ログ
+    new DailyRotateFile({
+      dirname: logDir,
+      filename: 'prisma-query-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'info',
+      maxFiles: '14d',
+      format: winston.format((info) => (info.type === 'prisma-query' ? info : false))(),
+    }),
+  ],
+});
