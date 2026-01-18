fd825fd05ceabd4882973d00fae48dbde1cf4fef
nakano <nakano2001a@gmail.com>
2026-01-18 12:29:52 +0900

    useCallback化

--- frontend/src/hooks/api/useView.ts
+++ frontend/src/hooks/api/useView.ts
@@ -1,17 +1,21 @@
 import type { ApiResponse } from '@shared/models/common';
 import { useLoadingCounter } from '@/hooks/ux/useLoadingCounter';
+import { useCallback } from 'react';
 
 export function useView<T>(viewFn: (id: string) => Promise<ApiResponse<T>>) {
   const { loading, start, end } = useLoadingCounter();
 
-  const view = async (id: string): Promise<ApiResponse<T>> => {
-    start();
-    try {
-      return await viewFn(id);
-    } finally {
-      end();
-    }
-  };
+  const view = useCallback(
+    async (id: string): Promise<ApiResponse<T>> => {
+      start();
+      try {
+        return await viewFn(id);
+      } finally {
+        end();
+      }
+    },
+    [viewFn, start, end],
+  );
 
   return { view, loading };
 }
