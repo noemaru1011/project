8f04fbb9221c997c7339c82b29ae442712c02dd5
nakano <nakano2001a@gmail.com>
2026-01-25 14:24:40 +0900

    仮

--- backend/src/features/history/controllers/historyController.ts
+++ backend/src/features/history/controllers/historyController.ts
@@ -26,9 +26,10 @@ export class HistoryController {
 
   searchByStartTimeHistories = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const datetimeStr = req.query.datetime as string;
-
-      const query = new Date(datetimeStr);
+      // validateQueryで既にDateオブジェクトに変換されている
+      // req.queryはParsedQs型だが、実際にはHistoryServerSearchInput型のデータが入っている
+      const validatedQuery = req.query as unknown as { datetime: Date };
+      const query = validatedQuery.datetime;
 
       const histories = await this.historyService.searchByStartTimeHistories(query);
       return res.status(200).json({
