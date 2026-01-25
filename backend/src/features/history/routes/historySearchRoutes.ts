c847c73ddaf4edd223572b9e57ceea3a48bc15f3
nakano <nakano2001a@gmail.com>
2026-01-25 14:40:40 +0900

    Revert "修正"
    This reverts commit 5dc17b77f4887b02b3f7c359b551ee6d58ccbb78.

--- backend/src/features/history/routes/historySearchRoutes.ts
+++ backend/src/features/history/routes/historySearchRoutes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { historyController } from '@/buildAppModules';
-import { validateBody } from '@/middleware/validateMiddleware';
+import { validateBody, validateQuery } from '@/middleware/validateMiddleware';
 import { StudentServerSearchSchema } from '@shared/models/student';
+import { HistoryServerSearchSchema } from '@shared/models/history';
 import { csrfMiddleware } from '@/middleware';
 
 const router = Router();
@@ -13,6 +14,10 @@ router.post(
   historyController.searchHistories,
 );
 
-router.get('/', historyController.searchByStartTimeHistories);
+router.get(
+  '/',
+  validateQuery(HistoryServerSearchSchema),
+  historyController.searchByStartTimeHistories,
+);
 
 export default router;
