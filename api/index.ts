ea75269d771166d952c7de1b1fc64533b977a9a8
nakano <nakano2001a@gmail.com>
2026-02-06 20:56:21 +0900

    パス修正

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,8 @@
-// api/index.ts
-import "module-alias/register";
-import app from "../backend/src/app.js"; // Expressのappインスタンスをインポート
+import * as moduleAlias from "module-alias";
+import path from "path";
+
+// これで addAlias が認識されます
+moduleAlias.addAlias("@shared", path.join(__dirname, "../shared"));
+
+import app from "../backend/src/app";
 export default app;
