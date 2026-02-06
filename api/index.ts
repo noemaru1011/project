4bcce9804c27073a9e61fc3760e0e80a42dd2398
nakano <nakano2001a@gmail.com>
2026-02-06 20:58:39 +0900

    パス適応

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,11 @@
 import * as moduleAlias from "module-alias";
 import path from "path";
 
-// これで addAlias が認識されます
+// 1. @shared の登録
 moduleAlias.addAlias("@shared", path.join(__dirname, "../shared"));
+
+// 2. @ の登録
+moduleAlias.addAlias("@", path.join(__dirname, "../backend/src"));
 
 import app from "../backend/src/app";
 export default app;
